bbd8cb17a4f1791cffe55dbab5f35f118856d2dc
mostafa <mostafanabil198@gmail.com>
2022-09-11 17:15:57 +0200

    add comments functionaliy and modal

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,31 @@
 class CommentsController < ApplicationController
-  before_action :set_post
+  before_action :set_post, only: [:create]
   def create 
     @comment = @post.comments.create(user: current_user, body: params[:comment_body])
+
+    respond_to do |format|
+      format.turbo_stream do 
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+          partial: "posts/post_comments",
+          locals: {post: @post}
+        )
+      end
+    end
   end
 
   def destroy 
     @comment = Comment.find(params[:id])
     if(@comment.user == current_user)
       @comment.destroy
+
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.remove(
+            "post#{@comment.post_id}ModalComment#{@comment.id}"
+          )
+        end
+      end
     end
   end
 
