f86f7ffcf81cb093a04f0ccab5ec7e53f688b035
mostafa <mostafanabil198@gmail.com>
2022-10-04 21:01:17 +0200

    Edit Profile Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone_number, :full_name, :profile_pic, :bio,:private])
+
   end
 end
