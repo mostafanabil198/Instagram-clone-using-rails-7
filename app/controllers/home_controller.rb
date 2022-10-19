7d01a78e0819421de609eace0bc216e12eb6f3e6
mostafa <mostafanabil198@gmail.com>
2022-10-19 21:59:42 +0200

    suggestions system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_suggestions
   def index
+  end
+
+
+  def set_suggestions
+    @suggestions = [current_user.followers]
+    [current_user.followers, current_user.followings].flatten.uniq.each do |f|
+      @suggestions.append([f.followers, f.followings])
+    end
+    @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten 
+    @suggestions = @suggestions.sample(5)
   end
 end
