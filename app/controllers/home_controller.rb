7b5bb4e3a4f2f9fab95f7ab18813ebf32d980e5b
mostafa <mostafanabil198@gmail.com>
2022-10-19 22:11:46 +0200

    Post feeds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
   before_action :set_suggestions
+  before_action :set_feeds
   def index
   end
 
+
+  private
+  def set_feeds
+    @feeds = Post.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
+  end
 
   def set_suggestions
     @suggestions = [current_user.followers]
